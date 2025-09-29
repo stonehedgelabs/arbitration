4fe17d398247af2a34026a80157854e7b168e470
ra0x3 <spam.rashad@protonmail.com>
2025-09-29 11:24:15 -0700

    Initial Commit

---
+++ arbitrationTests/arbitrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  arbitrationTests.swift
+//  arbitrationTests
+//
+//  Created by Rashad on 9/29/25.
+//
+
+import Testing
+
+struct arbitrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
